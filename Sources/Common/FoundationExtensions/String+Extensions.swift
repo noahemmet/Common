e3c34f3df3cf1b45bfba47916f37c746470610be
Noah Emmet <noah@noahemmet.com>
2019-07-20 03:03:37 -0700

    ranges(of substring

--- Sources/Common/FoundationExtensions/String+Extensions.swift
+++ Sources/Common/FoundationExtensions/String+Extensions.swift
@@ -33,6 +33,17 @@ public extension String {
 		return String(prefix(upTo: index))
 	}
 	
+	// https://stackoverflow.com/a/47220964
+    func ranges(of substring: String, options: CompareOptions = [], locale: Locale? = nil) -> [Range<Index>] {
+        var ranges: [Range<Index>] = []
+        while ranges.last.map({ $0.upperBound < self.endIndex }) ?? true,
+            let range = self.range(of: substring, options: options, range: (ranges.last?.upperBound ?? self.startIndex)..<self.endIndex, locale: locale)
+        {
+            ranges.append(range)
+        }
+        return ranges
+    }
+	
 	func prepending(_ prefix: String) -> String {
 		return prefix + self
 	}
